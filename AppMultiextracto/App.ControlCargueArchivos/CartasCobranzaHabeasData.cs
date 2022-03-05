824c8aa882a392824993aa8364ebee92fd02ae6a
Ivanm2b <92653855+Ivanm2b@users.noreply.github.com>
2022-03-04 21:09:59 -0500

    04-03-2022-21-09 IM
    Carga Habeas Excel, y Bases Estado de Cuenta Asociados y Terceros

--- AppMultiextracto/App.ControlCargueArchivos/CartasCobranzaHabeasData.cs
+++ AppMultiextracto/App.ControlCargueArchivos/CartasCobranzaHabeasData.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DLL_Utilidades;
+using Helpers = App.Controlnsumos.Helpers;
+using var = App.Variables.Variables;
 
 namespace App.ControlCargueArchivos
 {
     public class CartasCobranzaHabeasData : ICargue
     {
+        private const string _producto = "CartasCobranzaHabeasData";
+
         public CartasCobranzaHabeasData(string pArchivo)
         {
             try
@@ -27,7 +31,47 @@ namespace App.ControlCargueArchivos
 
         public void CargueArchivoDiccionario(string pArchivo)
         {
-            
+            string llaveCruce = string.Empty;
+            List<string> archivo = Helpers.ConvertirExcel(pArchivo);
+
+            foreach (string linea in archivo)
+            {
+                if (linea.Split('|')[0].Trim().ToUpper() != "FECHA")
+                {
+                    llaveCruce = linea.Split('|')[1].Trim();
+                    if (!var.DiccionarioExtractos.ContainsKey(llaveCruce))
+                    {
+                        var.DiccionarioExtractos.Add(llaveCruce, new Dictionary<string, Variables.DatosExtractos>
+                            {
+                                {_producto, new Variables.DatosExtractos
+                                {
+                                    Separador = '|',
+                                    Extracto = new List<string>(){ linea}
+
+                                }
+                                }
+                            });
+                    }
+                    else
+                    {
+                        if (!var.DiccionarioExtractos[llaveCruce].ContainsKey(_producto))
+                        {
+                            var.DiccionarioExtractos[llaveCruce].Add(_producto, new Variables.DatosExtractos
+                            {
+                                Separador = '|',
+                                Extracto = new List<string>() { linea }
+                            });
+                        }
+                        else
+                        {
+                            var.DiccionarioExtractos[llaveCruce][_producto].Extracto.Add(linea);
+                        }
+
+                    }
+                }
+
+            }
+
         }
 
         public void Ejecutar(string pArchivo)
