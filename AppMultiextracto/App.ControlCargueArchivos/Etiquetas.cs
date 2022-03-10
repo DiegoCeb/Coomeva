c6e54229c759cfe2ebc62e01dc2193e387a1c237
Ivanm2b <92653855+Ivanm2b@users.noreply.github.com>
2022-03-09 19:09:30 -0500

    09-03-2022-19-09 IM
    Sincronización Main

--- AppMultiextracto/App.ControlCargueArchivos/Etiquetas.cs
+++ AppMultiextracto/App.ControlCargueArchivos/Etiquetas.cs
@@ -4,14 +4,26 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using var = App.Variables.Variables;
 using DLL_Utilidades;
 
 namespace App.ControlCargueArchivos
 {
+    /// <summary>
+    /// Clase que se encarga de cargar el archivo de Etiquetas
+    /// </summary>
     public class Etiquetas : ICargue
     {
+        private const string _productoMail = "EtiquetaMail";
+        private const string _productoFisico = "EtiquetaFisico";
+
+        /// <summary>
+        /// Constructor de clase.
+        /// </summary>
+        /// <param name="pArchivo">ruta del archivo a cargar</param>
         public Etiquetas(string pArchivo)
         {
+            #region Etiquetas
             try
             {
                 Ejecutar(pArchivo);
@@ -22,23 +34,71 @@ namespace App.ControlCargueArchivos
                 System.Threading.Thread.Sleep(2000);
                 Utilidades.EscribirLog(ex.Message, Utilidades.LeerAppConfig("RutaLog"));
                 Environment.Exit(1);
-            }
+            } 
+            #endregion
         }
 
+        /// <summary>
+        /// Metodo Encargado de cargar al diccionario Principal los datos PUROS, solo con limpieza.
+        /// </summary>
+        /// <param name="pArchivo">ruta del archivo a cargar</param>
         public void CargueArchivoDiccionario(string pArchivo)
         {
+            #region CargueArchivoDiccionario
             StreamReader lector = new StreamReader(pArchivo, Encoding.Default);
 
             string linea = string.Empty;
+            string producto = string.Empty;
+
+            if (Path.GetFileNameWithoutExtension(pArchivo).Split('_').ElementAt(0).Last() == 'E')
+            {
+                producto = _productoMail;
+            }
+            else
+            {
+                producto = _productoFisico;
+            }
 
             while ((linea = lector.ReadLine()) != null)
             {
+                string llaveCruce = $"{linea.Substring(143, 6)}{linea.Substring(151, 5)}".TrimStart('0');
 
+                if (var.DiccionarioExtractos.ContainsKey(llaveCruce))
+                {
+                    if (var.DiccionarioExtractos[llaveCruce].ContainsKey(producto))
+                    {
+                        var.DiccionarioExtractos[llaveCruce][producto].Extracto.Add(linea);
+                    }
+                    else
+                    {
+                        var.DiccionarioExtractos[llaveCruce].Add(producto, new Variables.DatosExtractos
+                        {
+                            Separador = 'P',
+                            Extracto = new List<string>() { linea }
+                        });
+                    }
+                }
+                else
+                {
+                    var.DiccionarioExtractos.Add(llaveCruce, new Dictionary<string, Variables.DatosExtractos>
+                        {
+                            { producto, new Variables.DatosExtractos
+                            {
+                                Separador = 'P',
+                                Extracto = new List<string>() { linea }
+                            } }
+                        });
+                }
             }
 
-            lector.Close();
+            lector.Close(); 
+            #endregion
         }
 
+        /// <summary>
+        /// Metodo que desencadena el cargue.
+        /// </summary>
+        /// <param name="pArchivo">ruta del archivo a cargar</param>
         public void Ejecutar(string pArchivo)
         {
             CargueArchivoDiccionario(pArchivo);
--- AppMultiextracto/App.ControlCargueArchivos/Fiducoomeva.cs
+++ AppMultiextracto/App.ControlCargueArchivos/Fiducoomeva.cs
@@ -4,14 +4,25 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using var = App.Variables.Variables;
 using DLL_Utilidades;
 
 namespace App.ControlCargueArchivos
 {
+    /// <summary>
+    /// Clase que se encarga de cargar el archivo de Fiducoomeva
+    /// </summary>
     public class Fiducoomeva : ICargue
     {
+        private const string _producto = "Fiducoomeva";
+
+        /// <summary>
+        /// Constructor de clase.
+        /// </summary>
+        /// <param name="pArchivo">ruta del archivo a cargar</param>
         public Fiducoomeva(string pArchivo)
         {
+            #region Fiducoomeva
             try
             {
                 Ejecutar(pArchivo);
@@ -23,22 +34,109 @@ namespace App.ControlCargueArchivos
                 Utilidades.EscribirLog(ex.Message, Utilidades.LeerAppConfig("RutaLog"));
                 Environment.Exit(1);
             }
+            #endregion
         }
 
+        /// <summary>
+        /// Metodo Encargado de cargar al diccionario Principal los datos PUROS, solo con limpieza.
+        /// </summary>
+        /// <param name="pArchivo">ruta del archivo a cargar</param>
         public void CargueArchivoDiccionario(string pArchivo)
         {
+            #region CargueArchivoDiccionario
             StreamReader lector = new StreamReader(pArchivo, Encoding.Default);
 
             string linea = string.Empty;
+            string llaveCruce = string.Empty;
+            List<string> temp = new List<string>();
+            bool extractoCompleto = false;
 
             while ((linea = lector.ReadLine()) != null)
             {
+                if (linea == "\u001a")
+                {
+                    continue;
+                }
 
+                if (linea.Substring(0, 3) == "001")
+                {
+                    extractoCompleto = false;
+
+                    if (temp.Count > 1)
+                    {
+                        extractoCompleto = true;
+                    }
+
+                    if (extractoCompleto)
+                    {
+                        llaveCruce = temp.ElementAt(0).Split(';').ElementAt(14).TrimStart('0').Trim();
+
+                        AgregarDiccionario(llaveCruce, temp);
+
+                        temp.Clear();
+                    }
+
+                    temp.Add(linea);
+                }
+                else
+                {
+                    temp.Add(linea);
+                }
+            }
+
+            //Ultimo Extracto
+            if (temp.Count > 1)
+            {
+                llaveCruce = temp.ElementAt(0).Split(';').ElementAt(14).TrimStart('0').Trim();
+
+                AgregarDiccionario(llaveCruce, temp);
             }
 
             lector.Close();
+            #endregion
         }
 
+        /// <summary>
+        /// Metodo que agrega al Dicionario General.
+        /// </summary>
+        /// <param name="pLlaveCruce">llave de cruce (Cedula)</param>
+        /// <param name="pTemp">Lista del extracto</param>
+        private void AgregarDiccionario(string pLlaveCruce, List<string> pTemp)
+        {
+            #region AgregarDiccionario
+            if (var.DiccionarioExtractos.ContainsKey(pLlaveCruce))
+            {
+                if (var.DiccionarioExtractos[pLlaveCruce].ContainsKey(_producto))
+                {
+                    var.DiccionarioExtractos[pLlaveCruce][_producto].Extracto.InsertRange(var.DiccionarioExtractos[pLlaveCruce][_producto].Extracto.Count, pTemp);
+                }
+                else
+                {
+                    var.DiccionarioExtractos[pLlaveCruce].Add(_producto, new Variables.DatosExtractos
+                    {
+                        Separador = ';',
+                        Extracto = new List<string>(pTemp)
+                    });
+                }
+            }
+            else
+            {
+                var.DiccionarioExtractos.Add(pLlaveCruce, new Dictionary<string, Variables.DatosExtractos>
+                        {
+                            { _producto, new Variables.DatosExtractos
+                            {
+                                Separador = ';',
+                                Extracto = new List<string>(pTemp)
+                            } }
+                        });
+            }
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que desencadena el cargue.
+        /// </summary>
+        /// <param name="pArchivo">ruta del archivo a cargar</param>
         public void Ejecutar(string pArchivo)
         {
             CargueArchivoDiccionario(pArchivo);
